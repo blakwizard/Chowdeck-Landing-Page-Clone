2ea521962f5a9ede1e4a972d38ec737918cd3e3a
Ibraheem <ultimateibizzy@gmail.com>
2024-12-18 20:26:22 +0100

    added some javascript for practice sake

--- chow.js
+++ chow.js
@@ -14,3 +14,13 @@
 
 // let arrayOutput = myArray[1];
 // console.log(arrayOutput);
+
+const myFunction=(itemOne, ...otheritem)=>{
+    let firstItem=itemOne;
+    let secondItem= otheritem;
+
+    let totalItem = firstItem + secondItem;
+    return totalItem;
+}
+
+console.log(myFunction(200,300));
