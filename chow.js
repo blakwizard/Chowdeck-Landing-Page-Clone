6565c90112e11e22f4a292dc4e4caeb2f8605eab
Ibraheem <ultimateibizzy@gmail.com>
2024-12-18 16:26:44 +0100

    pushing desktop view of chowdeck clone project to github

---
+++ chow.js
@@ -0,0 +1,16 @@
+
+// let customerInfo = {
+//     customerName: "Ibraheem",
+//     customerAge: 25,
+//     occupation: "Product Designer"
+// }
+// console.log(customerInfo)
+// let myArray= [
+//     "Shoes",
+//     "Bags",
+//     "Babes",
+//     "Phones"
+// ];
+
+// let arrayOutput = myArray[1];
+// console.log(arrayOutput);
