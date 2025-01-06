3e86962401dac29abd8c0698260e7532115da4ec
Ibraheem <ultimateibizzy@gmail.com>
2025-01-06 09:34:07 +0100

    practiced some js in file

--- chow.js
+++ chow.js
@@ -1,10 +1,12 @@
-
+//Object literal
 // let customerInfo = {
 //     customerName: "Ibraheem",
 //     customerAge: 25,
 //     occupation: "Product Designer"
 // }
 // console.log(customerInfo)
+
+//Array 
 // let myArray= [
 //     "Shoes",
 //     "Bags",
@@ -12,15 +14,32 @@
 //     "Phones"
 // ];
 
+
 // let arrayOutput = myArray[1];
 // console.log(arrayOutput);
 
-const myFunction=(itemOne, ...otheritem)=>{
-    let firstItem=itemOne;
-    let secondItem= otheritem;
+// const myFunction=(itemOne, ...otheritem)=>{
+//     let firstItem=itemOne;
+//     let secondItem= otheritem;
 
-    let totalItem = firstItem + secondItem;
-    return totalItem;
+//     let totalItem = firstItem + secondItem;
+//     return totalItem;
+// }
+
+// console.log(myFunction(200,300));
+
+// let x = -1
+// if(x>0){
+//     console.log("positive");
+    
+// }else if(x < 0){
+//     console.log("Negative");
+    
+// }else{
+//     console.log("zero")
+// }
+function greet(name= "Guest"){
+    return `Hello, ${name}`
 }
 
-console.log(myFunction(200,300));
+console.log(greet())
